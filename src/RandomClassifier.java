11d085a37f3f53ec25217e81d9240960656f65e8
Muntaser Ahmed <muntaserahmed@d-172-25-98-222.bootp.Virginia.EDU>
2015-04-24 15:47:35 -0400

    tentative hypo and cost functions

--- src/RandomClassifier.java
+++ src/RandomClassifier.java
@@ -7,12 +7,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.SocketPermission;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.FileReader;
 
 public class RandomClassifier extends Classifier {
     public String names_file;
     public HashMap<String, String> fields;
+    public double[] thetaValues;
+    double theta;
 
     /**
      * Constructor: Initializes the classifier by reading in the .names file which lets the object
@@ -20,6 +23,8 @@ public class RandomClassifier extends Classifier {
      */
     public RandomClassifier(String namesFilepath){
         this.names_file = namesFilepath;
+        this.thetaValues = new double[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        this.theta = 0;
     }
 
     public void read_name_file() {
@@ -68,6 +73,31 @@ public class RandomClassifier extends Classifier {
      * standard output, one classification per line. Nothing else should be printed to standard output
      */
     public void makePredictions(String testDataFilepath){}
+
+    public double costFunction(ArrayList<int[]> dataList) {
+
+        // Last element: if 1, greater than 50k, if 0, less than 50k
+        if (dataList == null || dataList.size() == 0) {
+            System.out.println("COULDN'T PARSE");
+        }
+
+        int m = dataList.size();
+        double sum = 0;
+
+        for (int i = 0; i < m; i++) {
+            int[] row = dataList.get(i);
+            int[] yVal = row[row.length]; // FIX/VERIFY
+            sum += Math.pow(hypothesisFunction(row - yVal), 2);
+        }
+        return (1 / (2 * m)) * sum;
+    }
+
+    public double hypothesisFunction(int[] row) {
+        return theta + thetaValues[0] * row[0] + thetaValues[1] * row[1] + thetaValues[2] * row[2] +
+                thetaValues[3] * row[3] + thetaValues[4] * row[4] + thetaValues[5] * row[5] + thetaValues[6] * row[6] +
+                thetaValues[7] * row[7] + thetaValues[8] * row[8] + thetaValues[9] * row[9] +
+                thetaValues[10] * row[10] + thetaValues[11] * row[11] + thetaValues[12] * row[12];
+    }
 
     public static void main(String [] args) {
         String file = "C:\\Users\\student\\Classifier\\Training_Data\\census.names";
